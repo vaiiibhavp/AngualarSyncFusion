15750f550832869f762f9794d35fae88fb6ce431
ManjeeriN <manjeerinawghare@gmail.com>
2021-11-18 11:29:02 +0530

    reorder and freeze

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { sampleData } from './datasource';
+import { TreeGridComponent, RowDDService, SelectionService,ColumnChooserService} from '@syncfusion/ej2-angular-treegrid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ RowDDService, SelectionService,ColumnChooserService ]
 })
 export class AppComponent implements OnInit {
   title = 'AngualarSyncFusion';
@@ -17,17 +19,21 @@ export class AppComponent implements OnInit {
   public freightrules!: Object;
   public pageSettings!: Object;
   public editparams!: Object;
+  public allowRowDragAndDrop : boolean = true;
+  public selectOptions!: Object;
+
 
   ngOnInit(): void {
     this.data = sampleData;
-
+    this.selectOptions = { type: 'Multiple' };
     this.editSettings = { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Dialog' };
-    this.toolbar = ['Add', 'Edit', 'Delete'];
+    this.toolbar = ['Add', 'Edit', 'Delete','ColumnChooser'];
     this.orderidrules = { required: true, number: true };
     this.customeridrules = { required: true };
     this.freightrules =  { required: true };
     this.editparams = { params: { popupHeight: '300px' }};
     this.pageSettings = { pageCount: 5};
+    this.allowRowDragAndDrop = true;
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { TreeGridModule } from '@syncfusion/ej2-angular-treegrid';
 import { PageService, SortService, FilterService, EditService, ToolbarService } from '@syncfusion/ej2-angular-treegrid';
-
+import { GridModule, FreezeService, SelectionService } from '@syncfusion/ej2-angular-grids';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -16,9 +15,15 @@ import { AppComponent } from './app.component';
     TreeGridModule,
     AppRoutingModule
   ],
-  providers: [PageService,
+  providers: [
+    PageService,
     SortService,
-    FilterService, EditService, ToolbarService],
+    FilterService, 
+    EditService,
+    ToolbarService,
+    FreezeService,
+    SelectionService
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
