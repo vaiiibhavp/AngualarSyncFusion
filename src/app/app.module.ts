7780a5dc724e451392650fb6e7e35e5001772b8f
Amol Narkhede <amolprometteur>
2021-11-22 13:21:11 +0530

    toolbar checkbox added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TreeGridModule } from '@syncfusion/ej2-angular-treegrid';
 import { PageService, SortService, FilterService, EditService, ToolbarService } from '@syncfusion/ej2-angular-treegrid';
 import { GridModule, FreezeService, SelectionService } from '@syncfusion/ej2-angular-grids';
+import { CheckBoxModule } from '@syncfusion/ej2-angular-buttons';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -13,8 +14,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     TreeGridModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CheckBoxModule
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [
     PageService,
     SortService,
